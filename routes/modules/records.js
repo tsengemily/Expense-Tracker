f9f5e081c84acc8c9518c310474d6f30c544606a
Emily <tsengemily818@gmail.com>
2020-11-29 17:52:36 +0800

    feat: add delete function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -59,4 +59,13 @@ router.post('/:id/edit', (req, res) => {
     .catch((error) => console.log(error))
 })
 
+//功能:刪除
+router.post('/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then((record) => record.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
 module.exports = router
